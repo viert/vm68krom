980df88b939bb92810b576f8ba2919fb1f064287
Pavel Vorobyov <aquavitale@yandex.ru>
2018-01-29 23:21:48 +0300

    started advanced console

--- putc.s
+++ putc.s
@@ -5,10 +5,17 @@
         .global set_cursor_pos
         .global set_color
 
-        .equ    text_start,         0x00780000
-        .equ    color_start,        0x007812C0
+        .equ    text_start,             0x00780000
+        .equ    second_line_start,      text_start + 80
+        .equ    color_start,            0x007812C0
+        .equ    second_line_col_start,  color_start + 80
+
         .equ    cursor_pos_var,     0x00010000
         .equ    cursor_color_var,   0x00010002
+        .equ    text_screen_width,  80
+        .equ    text_screen_height, 60
+        .equ    words_to_move,      80 * 59 / 4 - 1
+        .equ    text_max_pos,       text_screen_height * text_screen_width
 putc:
         ; # d0.b should contain a character
         move.l  %a0, -(%sp)
@@ -25,6 +32,31 @@ putc:
         move.b  %d0, (%a0)
 
         add.w   #1, cursor_pos_var
+        cmpi.w  #text_max_pos, cursor_pos_var
+        bmi     pos_no_overflow
+
+        ; # moving up one line
+        movem.l %a0-%a3/%d0, -(%sp)
+
+        move.l  #second_line_start, %a1
+        move.l  #text_start, %a0
+
+        move.l  #second_line_col_start, %a3
+        move.l  #color_start, %a2
+
+        move.l  #words_to_move, %d0
+
+        ; # copy loop
+text_move_loop:
+        move.l  (%a1)+, (%a0)+
+        move.l  (%a3)+, (%a2)+
+        dbf     %d0, text_move_loop
+
+        movem.l (%sp)+, %a0-%a3/%d0
+
+        addi.w  -#80, cursor_pos_var
+
+pos_no_overflow:
         move.l  (%sp)+, %a0
         rts
 
