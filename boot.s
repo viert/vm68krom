fdd70e562340f4073fd441708e15abd0d4084304
Pavel Vorobyov <aquavitale@yandex.ru>
2018-01-30 16:11:47 +0300

    cdecl compliant read_sync function

--- boot.s
+++ boot.s
@@ -11,34 +11,44 @@
         .equ    boot_prg_start, 0x00001200
         .equ    hdd_cmd_read,   0x02
 
-read_bootloader:
-        move.l      #hdd_ready, %a0
-        move.l      %a0, user_exc_int5 ; # setting hdd callback
+; # void read_sync(int sector_number, int sector_count, char* buffer)
+; #
 
-        move.l      #boot_prg_start, %a0 ; # where to store read data
-        eor.l       %d0, %d0 ; # d0 is the sector index, set to 0
-        move.l      #0x01, %d3 ; # d3 is a counter (2 sectors)
+read_sync:
+        movem.l     %a0-%a1/%d0-%d3, -(%sp)
+        move.l      28(%sp), %d0            ; # sector number
+        move.l      32(%sp), %d3            ; # sectors count
+        subq        #1, %d3                 ; # m68k counters count down to -1
+        move.l      36(%sp), %a0            ; # buffer start
+        moveq       #hdd_cmd_read, %d1      ; # cmd_read cache
+        move.l      #hdd_ready, user_exc_int5
 
 read_sector_incr_loop:
-        bsr         int_mask
+        andi.w      #0b1111100011111111, %sr ; # enable interrupts
         move.l      %d0, hdd_cmdarg
-        moveq       #hdd_cmd_read, %d1
         move.b      %d1, hdd_cmd
 
 read_sector_halt:
         bra         read_sector_halt
 
 hdd_ready:
-        addq        #6, %sp ; # won't return from this interrupt
+        addq        #6, %sp                 ; # not gonna return from this interrupt
         move.l      #hdd_buffer, %a1
-        move.l      #0x7F, %d2 ; # 0x80 * 4 = 512 bytes to copy
-        
+        move.l      #0x7F, %d2              ; # 0x80 * 4 = 512 bytes to copy
 sector_copy_loop:
         move.l      (%a1)+, (%a0)+
         dbf         %d2, sector_copy_loop
-
-        addq        #1, %d0 ; # next sector
+        addq        #1, %d0                 ; # next sector
         dbf         %d3, read_sector_incr_loop
+        
+        movem.l     (%sp)+, %a0-%a1/%d0-%d3
+        rts
+
+read_bootloader:
+        move.l      #boot_prg_start, -(%sp) ; # read_sync
+        move.l      #2, -(%sp)              ; # 2 sectors
+        move.l      #0, -(%sp)              ; # starting from sector 0
+        bsr         read_sync
         rts
 
 int_mask:
